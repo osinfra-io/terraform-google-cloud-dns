f5688efbd46744499c013c339dcb8ebd9d4663b7
Brett Curtis <brett@osinfra.io>
2024-02-24 11:40:55 -0500

    Align tests and Infracost tags (#10)

--- test/integration/default_cloud_dns/controls/gcp_inspec.rb
+++ test/integration/default_cloud_dns/controls/gcp_inspec.rb
@@ -1,3 +1,9 @@
+# Chef InSpec
+# https://www.chef.io/inspec
+
+# Since this is the default test, we want to test as much as possible here and not be
+# redundant in the other tests.
+
 project = input('project')
 
 control 'dns_managed_zone' do
