ecdc19dfacd83925ad0e8dac2ddbc49cc09b62c7
Brett Curtis <brett@osinfra.io>
2024-04-14 18:33:49 -0400

    Deprecation of Kitchen-Terraform (#19)

--- fixtures/default/main.tf
+++ fixtures/default/main.tf
@@ -3,7 +3,7 @@ module "private_test" {
   # This module will be consumed using the source address of the github repo and not the "../../../global" used in this test.
   # source = "github.com/osinfra-io/terraform-google-cloud-dns//global?ref=v0.0.0"
 
-  source = "../../../global"
+  source = "../../global"
 
   dns_name = "gcp-priv.osinfra.io."
 
@@ -15,7 +15,7 @@ module "private_test" {
   }
 
   name                               = "gcp-priv-osinfra-io"
-  private_visibility_config_networks = ["https://www.googleapis.com/compute/v1/projects/${var.project}/global/networks/kitchen-vpc"]
+  private_visibility_config_networks = ["https://www.googleapis.com/compute/v1/projects/${var.project}/global/networks/terraform-test-vpc"]
   project                            = var.project
   visibility                         = "private"
 }
@@ -25,7 +25,7 @@ module "public_test" {
   # This module will be consumed using the source address of the github repo and not the "../../../global" used in this test.
   # source = "github.com/osinfra-io/terraform-google-cloud-dns//global?ref=v0.0.0"
 
-  source = "../../../global"
+  source = "../../global"
 
   dns_name = "gcp.osinfra.io."
 
