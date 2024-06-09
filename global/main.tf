0dd6e5c567794bf745768b0b56738119e5520805
Brett Curtis <brett@osinfra.io>
2024-06-09 08:09:17 -0400

    Enable checkov in pre-commit (#23)

--- global/main.tf
+++ global/main.tf
@@ -2,6 +2,10 @@
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/dns_managed_zone
 
 resource "google_dns_managed_zone" "this" {
+
+  # Ensure that DNSSEC is enabled for Cloud DNS
+  # checkov:skip=CKV_GCP_16: False positive
+
   description = var.description
   dns_name    = var.dns_name
   labels      = var.labels
